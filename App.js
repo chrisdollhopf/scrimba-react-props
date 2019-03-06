08fd5c59022a51cc6b4cd5409432de1653adde9a
chrisdollhopf <42962393+chrisdollhopf@users.noreply.github.com>
2019-03-06 10:58:45 +0100

    second commit
    new file structure and contents, App.js  contains a repeated structure that needs to be refactored.

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+function App() {
+    return (
+        <div className="contacts">
+
+            <div className="contact-card">
+                <img src="http://placekitten.com/300/200"/>
+                <h3>Mr. Whiskerson</h3>
+                <p>Phone: (000) 111-2222</p>
+                <p>Email: mr.whiskaz@catnap.meow</p>
+            </div>
+            
+            <div className="contact-card">
+                <img src="http://placekitten.com/g/300/200"/>
+                <h3>Mr. Benji</h3>
+                <p>Phone: (111) 222-3333</p>
+                <p>Email: mr.benji@catnap.meow</p>
+            </div>
+
+            <div className="contact-card">
+                    <img src="http://placekitten.com/300/200"/>
+                <h3>Mr. Tammy</h3>
+                <p>Phone: (222) 333-4444</p>
+                <p>Email: mr.tammy@catnap.meow</p>
+            </div>
+
+            <div className="contact-card">
+                <img src="http://placekitten.com/g/300/200"/>
+                <h3>Mr. Ginger</h3>
+                <p>Phone: (333) 444-5555</p>
+                <p>Email: mr.ginger@catnap.meow</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default App
