38009b8a2e4e5368bfe5e4f3d52e35c1baf43e21
chrisdollhopf <42962393+chrisdollhopf@users.noreply.github.com>
2019-03-06 11:52:26 +0100

    fourth commit
    using the JS {}'s to accept properties, "props" is the placeholder word or parameter for the syntax that follows. Could be "whats" if preferred.

--- ContactCard.js
+++ ContactCard.js
@@ -1,26 +1,17 @@
 import React from "react"
 
-/* Introduce a function paramter called props (can be called whatever
-but is typically called props short for properties
-
-Props is an object thhat has properties of that object, each one being
-a property that you passed in (in this case the App.js "contacts" or 
-rather the name that you passed in */
-
 function ContactCard(props) {
-
-/*     console.log(props) logs 4 console.log entries because we have at this
-    point 4 instances of the ContactCard component and the props are an
-    object with a name, imgUrl, email and phone properties.  */
-    
     return (
         <div className="contact-card">
-            <img src="http://placekitten.com/300/200"/>
-            <h3>Mr. Whiskerson</h3>
-            <p>Phone: (000) 111-2222</p>
-            <p>Email: mr.whiskaz@catnap.meow</p>
+            <img src={props.imgUrl}/>
+            <h3>{props.name}</h3>
+            {/* Leaving the Phone: space as a hard coded value */}
+            <p>Phone: {props.phone}</p>
+            <p>Email: {props.email}</p>
         </div>
     )
 }
+
+/* The above block of code is now a component that is now reusable. */
 
 export default ContactCard
